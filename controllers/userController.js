b1711388331c6607c637324fc55931058764177a
Patrick Hennessey <pjhennessey89@gmail.com>
2024-09-30 11:40:03 -0500

    Added more routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -70,7 +70,76 @@ module.exports = {
         }
     },
 
-    
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.userId);
 
+            if (!user) {
+                return res.status(404).json(
+                    {
+                        message: 'No user with this ID.'
+                    }
+                )
+            }
 
+            await Thought.deleteMany({
+                username: user.username
+            });
+
+            res.json({
+                message: 'User deleted.'
+            })
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    async addFriend(req, res) {
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.userId,
+                {
+                    $addToSet: {
+                        friends: req.params.friendId
+                    }
+                },
+                { new: true }
+            )
+                .populate('friends');
+
+            if (!user) {
+                return res.status(404).json(
+                    {
+                        message: 'No user with this ID.'
+                    }
+                )
+            }
+
+            res.json(user);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    },
+
+    async removeFriend(req, res) {
+        try {
+            const user = await User.findByIdAndUpdate(
+                req.params.userId,
+                { $pull: { friends: req.params.friendId } },
+                { new: true }
+            ).populate('friends');
+
+            if (!user) {
+                return res.status(404).json(
+                    {
+                        message: 'No user with this ID.'
+                    }
+                )
+            }
+
+            res.json(user);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
 }
